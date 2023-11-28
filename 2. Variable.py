b36fa764a6a2d6f71ac9045efe5e50bb12a34759
Asad-Zaidi <syedasad1410@gmail.com>
2023-11-28 22:25:05 +0500

    2nd Python Program

--- 2. Variable.py
+++ 2. Variable.py
@@ -22,3 +22,30 @@ my_variable = 5
 another_variable = "Hello"
 print(my_variable)
 print(another_variable)
+
+#variables
+name = "John Doe"
+age = 36
+country = "India"
+city = "Delhi"
+
+#printing variables
+
+print(name)
+print(age)
+print(country)
+print(city)
+
+#arithmetic operations
+
+sum = age + 2
+print(sum)
+difference = age - 7
+print(difference)
+product = age * 2
+print(product)
+quotient = age / 2
+print(quotient)
+remainder = age % 7
+print(remainder)
+
