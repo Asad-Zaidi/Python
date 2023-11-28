0a629d81ef86b7fa4293c3ae06afef444194f7a5
Asad-Zaidi <syedasad1410@gmail.com>
2023-11-28 22:27:24 +0500

    Updated

--- 1. Intro.py
+++ 1. Intro.py
@@ -1,2 +1,19 @@
 print("Hello World")
 print("I am Syed Asad Jamil. This is my first Python Program")
+
+#introduction
+
+name = "Syed Asad Jamil"
+age = 21
+city = "Lahore, Pakistan"
+country = "Pakistan"
+hobby = ["Reading", "Writing", "Coding"]
+
+#Print the introduction 
+
+print(name)
+print(age)
+print(city)
+print(country)
+print(hobby)
+
