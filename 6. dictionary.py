d209b46b66803292cb71dbe5b6c867a67cdc7f01
Asad-Zaidi <syedasad1410@gmail.com>
2024-01-05 22:19:02 +0500

    Python

---
+++ 6. dictionary.py
@@ -0,0 +1,11 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["model"])
+print(len(thisdict))
+
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
