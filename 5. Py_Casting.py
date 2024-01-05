c99847947e1b5e3436de06dd9c99bacb898b09e2
Asad-Zaidi <syedasad1410@gmail.com>
2024-01-05 22:14:36 +0500

    Python

--- 5. Py_Casting.py
+++ 5. Py_Casting.py
@@ -4,3 +4,25 @@ y = int(2.8) # y will be 2
 z = int("3")
 
 print (x,y,z)
+
+# Float
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+print (x,y,z,w)
+
+# String
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+print (x,y,z)
+
+# Complex numbers
+x = complex(1)     # x is (1+0j)
+y = complex(2, 3)  # y is (2+3j)
+
+print (x,y)
