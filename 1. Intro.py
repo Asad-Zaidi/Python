bd856b0e24dcfde9698ec404263818e91d4b971b
Asad-Zaidi <syedasad1410@gmail.com>
2024-01-05 22:09:18 +0500

    Python

--- 1. Intro.py
+++ 1. Intro.py
@@ -16,3 +16,39 @@ print("Age: " + str(AGE))
 print("City: " + CITY)
 print("Country: " + COUNTRY)
 print("Hobby: " + str(HOBBY))
+
+
+# Many Values to Multiple Variables
+
+x,y,z = "One", "Two", "Three"
+print(x,y,z)
+
+
+# Unpack a Collection
+Numbers = ["One", "Two", "Three"]
+
+x,y,z = Numbers
+print(x,y,z)
+
+
+# Output Variables
+
+X = "Python "
+Y = "is "
+Z = "Awesome"
+print(X + Y + Z)
+
+X = 5
+Y = 10
+print(X+ Y)
+
+# Global Variable
+
+X = "Awesome"
+def display():
+    """
+    This function prints the string "Python is Awesome".
+    """
+    print("Python is " + X)
+
+display()
