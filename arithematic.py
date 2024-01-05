3fa76d282458f4e421e7286568db7b5f79cad546
Asad-Zaidi <syedasad1410@gmail.com>
2024-01-05 22:32:09 +0500

    Python

---
+++ arithematic.py
@@ -0,0 +1,20 @@
+"""
+This is a module docstring.
+
+It provides an overview of the module's purpose and functionality.
+"""
+
+num1 = int(input("Enter Number 1 : "))
+num2 = int(input("Enter Number 2 : "))
+
+addition = num1 + num2
+print("Sum = ", addition)
+
+substraction = num1 - num2
+print("Substraction = ", substraction)
+
+multiplication = num1 * num2
+print("Multiplication = ", multiplication)
+
+division = num1 / num2
+print("Division = ", division)
